3b762e55bebe29fc2ec7d75af77336ba1192f5c9
Felix Gnass <fgnass@gmail.com>
2024-12-30 15:44:00 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import BankSelector from "./components/BankSelector";
 import Controls from "./components/Controls";
 import LEDMatrix from "./components/LEDMatrix";
+import DeviceControls from "./components/DeviceControls";
 
 export default function App() {
   return (
     <div class="min-h-screen bg-gray-900 text-white p-8">
       <div class="max-w-4xl mx-auto space-y-8">
-        <BankSelector />
+        <div class="flex items-center justify-between">
+          <BankSelector />
+          <DeviceControls />
+        </div>
         <LEDMatrix />
         <Controls />
       </div>
